4013b9fc8f85cd9d40c5ae10c93289058cdf36bc
Gutinnho <glunardi99@gmail.com>
2022-05-11 10:24:29 -0300

    Adicionando a mudança do Nav no OnScroll e as cores customizáveis

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,17 @@
+window.addEventListener('scroll', onScroll);
+
+function onScroll() {
+   shwNavBarOnScroll();
+}
+
+function shwNavBarOnScroll() {
+   if (scrollY > 12.5) {
+      navigation.classList.add('scroll');
+   } else {
+      navigation.classList.remove('scroll');
+   }
+}
+
 function openMenu() {
    document.body.classList.add('menu-expanded');
 }
